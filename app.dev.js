b540910f311741f0123156ed0a7ca4fa53ec8066
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2017-03-21 00:27:56 +0100

    Add dev script

---
+++ app.dev.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const Promise = require('bluebird')
+const express = require('express')
+const request = Promise.promisifyAll(require('request'))
+
+const app = express()
+
+app.use(express.static(path.join(__dirname,'static')))
+
+app.get('/metadata',(req,res,next) => {
+  res.send({
+    environment: {
+      name: 'Test env'
+    }
+  })
+})
+
+app.get('/services',(req,res,next) => {
+  res.send([
+    {name: 'Service 1', status: 'ok'},
+    {name: 'Service 2', status: 'warning', statusMessage: 'unhealthy', statusDescription: 'degraded performance'},
+    {name: 'Service 3', status: 'danger', statusMessage: 'error', statusDescription: 'something bad happened'}
+  ])
+})
+
+const port = process.env.PORT || 3001
+app.listen(port,(err) => {
+  if(err) console.error(`failed to start, err: ${err.message} (port: ${port})`);
+  else console.log(`listening on port ${port}`)
+})
