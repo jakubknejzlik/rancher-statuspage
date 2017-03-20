76a6342e7f66874fa5895caf0119eb9a0e5ad232
letrungkien0210 <letrungkien0210@gmail.com>
2017-03-21 05:43:43 +0700

    Implmeneted on Front-end: + Load data from API + Set interval.

---
+++ static/app.js
@@ -0,0 +1,101 @@
+class GetInfo extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            metadata: {},
+            loading: true
+        };
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:3000/metadata')
+            .then(res => {
+                console.log(res.data);
+                this.setState({
+                    metadata: res.data,
+                    loading: false
+                });
+            });
+    }
+
+    renderLoading(){
+        return <div>Loading ...</div>;
+    }
+
+    render(){
+        return(
+            <div className="header clearfix">
+                <h3 className="text-muted">{ this.state.loading ? this.renderLoading() : this.state.metadata.environment.name }</h3>
+            </div>
+        );
+    }
+}
+
+class Fetch extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            services: [],
+            loading: true
+        };
+    }
+
+    componentDidMount(){
+        this.timerID = setInterval(
+            () => this.getData(),
+            5000
+        );
+    };
+
+    renderLoading(){
+        return <div>Loading ...</div>;
+    }
+
+    renderServices(){
+        return (
+            <ul className ="list-group list-group-flush">
+                    {this.state.services.map(service =>
+                        <li className ="list-group-item d-flex justify-content-between">
+                            <span>{service.name}</span>
+                            <span className ="badge badge-success">{service.status}</span>
+                        </li>
+                    )}
+                </ul>
+        );
+    }
+
+    getData(){
+        axios.get('/services')
+            .then(res => {
+                console.log(res.data);
+                this.setState({
+                    services: res.data,
+                    loading: false
+                });
+            });
+    }
+
+    render(){
+        return (
+            <div className ="card">
+                { this.state.loading ? this.renderLoading() : this.renderServices()}
+            </div>
+        )
+    };
+}
+
+function App(){
+    return (
+        <div>
+           <GetInfo/>
+           <Fetch/>
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
